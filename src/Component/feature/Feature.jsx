cb38faa4c59c3fa70b656d6500548f4f30ab9354
dhruvisonani123 <dhruvisonaniid@gmail.com>
2025-01-28 13:25:52 +0530

    commit feature

--- src/Component/feature/Feature.jsx
+++ src/Component/feature/Feature.jsx
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from "react";
 import "../feature/feature.css";
 import feature from "../../assets/featurebg.png";
 import AOS from "aos";
+import multi from "../../assets/multil.svg";
+import aipower from "../../assets/aipower.svg";
+import leadf from "../../assets/leadf.svg";
+import persnalf from "../../assets/persnalmessage.svg"
+import voice from "../../assets/voicefirst.svg"
+import socialw from "../../assets/socialw.svg"
 import "aos/dist/aos.css";
 const Feature = () => {
   const [expandedFeature, setExpandedFeature] = useState("voice-over"); // Default card expanded
@@ -22,22 +28,28 @@ const Feature = () => {
 
   return (
     <div className="feature-1380">
-      <div className="feature-1320"  >
+      <div className="feature-1320">
         <div className="feature-container-home">Exclusive Features!</div>
-        <div className="exclusive-container" data-aos="fade-up" data-aos-duration="1500" data-aos-offset="200">
+        <div
+          className="exclusive-container"
+          data-aos="fade-up"
+          data-aos-duration="1500"
+          data-aos-offset="200"
+        >
           {/* Voice Over - Open by default */}
           <div
             className={`voice-over ${isExpanded("voice-over") ? "active" : ""}`}
             onMouseEnter={() => handleClick("voice-over")}
-           
           >
-            <img
-              src={feature}
-              alt="Feature background"
+            <div
+             
               className={`feature-image-home ${
                 isExpanded("voice-over") ? "expanded" : ""
               }`}
             />
+                   <div className="img-overlay-flex">
+              <img className="imgexp" src={voice} alt="" srcset="" />
+            </div>
             <div className="feature-left-padding">
               <p className="overlay-text">Voice Over</p>
               <p className="personalized">
@@ -55,13 +67,14 @@ const Feature = () => {
             }`}
             onMouseEnter={() => handleClick("multilanguage")}
           >
-            <img
-              src={feature}
-              alt="Feature background"
+            <div
               className={`feature-image-home ${
                 isExpanded("multilanguage") ? "expanded" : ""
               }`}
             />
+            <div className="img-overlay-flex">
+              <img className="imgexp" src={multi} alt="" srcset="" />
+            </div>
             <p className="overlay-text">Multilanguage Support</p>
             <p className="personalized">
               Connect globally with our multilanguage support feature.
@@ -77,13 +90,16 @@ const Feature = () => {
             }`}
             onMouseEnter={() => handleClick("lead-finder")}
           >
-            <img
+            <div
               src={feature}
               alt="Feature background"
               className={`feature-image-home ${
                 isExpanded("lead-finder") ? "expanded" : ""
               }`}
             />
+            <div className="img-overlay-flex">
+              <img className="imgexp" src={leadf} alt="" srcset="" />
+            </div>
             <p className="overlay-text">Lead finder</p>
             <p className="personalized">
               Easily discover and connect with potential clients using our lead
@@ -97,13 +113,16 @@ const Feature = () => {
             className={`voice-over ${isExpanded("ai-content") ? "active" : ""}`}
             onMouseEnter={() => handleClick("ai-content")}
           >
-            <img
+            <div
               src={feature}
               alt="Feature background"
               className={`feature-image-home ${
                 isExpanded("ai-content") ? "expanded" : ""
               }`}
             />
+             <div className="img-overlay-flex">
+              <img className="imgexp" src={aipower} alt="" srcset="" />
+            </div>
             <p className="overlay-text">AI-powered content suggestions</p>
             <p className="personalized">
               Boost your communication with AI-powered content suggestions. Get
@@ -119,13 +138,16 @@ const Feature = () => {
             }`}
             onMouseEnter={() => handleClick("personalized-messages")}
           >
-            <img
+            <div
               src={feature}
               alt="Feature background"
               className={`feature-image-home ${
                 isExpanded("personalized-messages") ? "expanded" : ""
               }`}
             />
+             <div className="img-overlay-flex">
+              <img className="imgexp" src={persnalf} alt="" srcset="" />
+            </div>
             <p className="overlay-text">Personalized messages</p>
             <p className="personalized">
               Create impactful connections with personalized messages. Tailor
@@ -141,13 +163,16 @@ const Feature = () => {
             }`}
             onMouseEnter={() => handleClick("social-widgets")}
           >
-            <img
+            <div
               src={feature}
               alt="Feature background"
               className={`feature-image-home ${
                 isExpanded("social-widgets") ? "expanded" : ""
               }`}
             />
+              <div className="img-overlay-flex">
+              <img className="imgexp" src={socialw} alt="" srcset="" />
+            </div>
             <p className="overlay-text">Social proof widgets</p>
             <p className="personalized">
               Embed social proof elements, such as recent testimonials, live
